4f6eff97ea29ee93325cdfe00d09f009538de609
zwzgg <39474340+zwzgg@users.noreply.github.com>
2018-05-21 11:21:15 +0800

    Add files via upload

---
+++ Jsonbean.java
@@ -0,0 +1,42 @@
+package com.wswx.bean;
+
+
+public class Jsonbean {
+    private String name;
+    private int age;
+    private String school;
+    private String[] major;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getSchool() {
+        return school;
+    }
+    
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    
+    public String[] getMajor() {
+        return major;
+    }
+    
+    public void setMajor(String[] major) {
+        this.major = major;
+    }
+    
+}
